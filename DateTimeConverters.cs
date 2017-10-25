2876cefbe5b90581e41bbdf937c15ebd81e5fd0b
Julie Lerman <julielerman@gmail.com>
2017-10-25 10:07:15 -0400

    add datetime converters. Tring to hide "cookie count" text

---
+++ DateTimeConverters.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace CookieBinge2.Converters
+{
+    public class DateFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null)
+                return null;
+
+            DateTime dt = DateTime.Parse(value.ToString());
+            return dt.ToString("M/dd/yy h:mm tt");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotSupportedException();
+        }
+
+    }
+}
